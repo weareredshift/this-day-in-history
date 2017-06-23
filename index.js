198143168a2f522104ac82857f799eaee1c7ea55
Trevor Alexander <alexander.trevor@mail.com>
2017-06-23 09:31:29 -0700

    So many dates

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const dateyRegex = /\d{4}|\d{2}|\d{1}|(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|N
 /* functions */
 function sendQuery() {
 	const query = document.getElementById('query');
-	if (query.value.length > 0) {	
+	if (query.value.length > 0) {
+		const resNode = document.getElementById('search-results');
+		resNode.innerHTML = "Loading...";
 		wiki().search(query.value)
 			.then(data => populateResults(data.results));
 	}
@@ -15,26 +17,49 @@ function sendQuery() {
 function populateResults(results) {
 	const resNode = document.getElementById('search-results');
 	const parsedDates = [];
-	results
-		.filter(Boolean)
-		.map(res => dateyRegex.exec(res))
-		.filter(Boolean)
-		.map(regexRes => regexRes[0])
-		.map(chrono.parseDate)
-		.filter(Boolean)
-		.map(pDate => {
-			parsedDates.push(pDate);
-			const el = fragmentFromString(`<li>${pDate}</li>`);
-			return el;
-		})
-		.forEach(frag => resNode.appendChild(frag));
+	resNode.innerHTML = '';
+	
+	Promise.all(
+		results
+			.filter(Boolean)
+			.map(res => wiki().page(res))
+			.map(pagePromise => pagePromise.then(pageData => pageData.content()))
+			.map(contentPromise => {
+				return contentPromise.then(content => {
+					content
+						.match(dateyRegex)
+						.map(match => new Date(Date.parse(match)))
+						.filter(date => date.toString() !== "Invalid Date")
+						.map(formatDate)
+						.map(pDate => {
+							parsedDates.push(pDate);
+							const el = fragmentFromString(`<li>${pDate}</li>`);
+							return el;
+						})
+						.forEach(frag => resNode.appendChild(frag));
+				});
+			})
+	).then(_ => Array
+				 .from(resNode.children)
+				 .forEach((node, ind) =>
+									node.addEventListener(
+										'click',
+										() => selectDate(parsedDates[ind].toString()),
+										false))
+				);
+}
 
-	Array.from(resNode.children)
-		.forEach((node, ind) =>
-						 node.addEventListener(
-							 'click',
-							 () => selectDate(parsedDates[ind].toString()),
-							 false));
+function formatDate(date) {
+	return `${date.getFullYear()}/${Math.ceil(Math.random() * 12)}/${Math.ceil(Math.random() * 31)}`;
+}
+
+function makeDateFromChunks(chunks) {
+	const dates = [];
+	chunks.reduce((almostDate, chunk) => {
+		
+	});
+
+	return dates;
 }
 
 function selectDate(dateText) {
