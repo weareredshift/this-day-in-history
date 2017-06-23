063c8c24d120b74ab59e43fa45b94807a59b24a4
Trevor Alexander <alexander.trevor@mail.com>
2017-06-23 11:00:44 -0700

    Added summaries, better visual

--- index.js
+++ index.js
@@ -18,35 +18,59 @@ function populateResults(results) {
 	const resNode = document.getElementById('search-results');
 	const parsedDates = [];
 	resNode.innerHTML = '';
+	let paraOffset = -7;
 	
 	Promise.all(
-		results
-			.filter(Boolean)
+		arrUnique(results
+							.filter(Boolean)
+							.slice(0,5))
 			.map(res => wiki().page(res))
 			.map(pagePromise => pagePromise.then(pageData => pageData.content()))
 			.map(contentPromise => {
 				return contentPromise.then(content => {
+					const firstPara = content.split('\n').slice(0,1).join(' ');
+					const excerptStart = Math.ceil(Math.random() * firstPara.length);
+					const excerptEnd = Math.max(
+						Math.ceil(Math.random() * firstPara.length) +
+							excerptStart, 30);
+					const summary = firstPara
+								.concat('...')
+								.slice(excerptStart, excerptEnd)
+								.concat('...');
+
+					resNode.appendChild(fragmentFromString(`<p><em>${summary}</em></p>`));
+					
 					content
 						.match(dateyRegex)
 						.map(match => new Date(Date.parse(match)))
 						.filter(date => date.toString() !== "Invalid Date")
+						.slice(0,5)
 						.map(formatDate)
-						.map(pDate => {
+						.forEach(pDate => {
 							parsedDates.push(pDate);
-							const el = fragmentFromString(`<li>${pDate}</li>`);
-							return el;
-						})
-						.forEach(frag => resNode.appendChild(frag));
+							const el = fragmentFromString(`<li><p><strong>${pDate}</strong></p></li>`);
+							resNode.appendChild(el);
+							resNode.children[resNode.children.length - 1].secretDate = pDate;
+						});
 				});
 			})
 	).then(_ => Array
 				 .from(resNode.children)
-				 .forEach((node, ind) =>
-									node.addEventListener(
-										'click',
-										() => selectDate(parsedDates[ind].toString()),
-										false))
+				 .forEach((node, ind) => {
+					 if (node.nodeName === "LI") {
+						 node.addEventListener(
+							 'click',
+							 () => {
+								 selectDate(node.secretDate);
+							 },
+							 false);
+					 }
+				 })
 				);
+}
+
+function arrUnique(arr) {
+	return Array.from(new Set(arr));
 }
 
 function formatDate(date) {
